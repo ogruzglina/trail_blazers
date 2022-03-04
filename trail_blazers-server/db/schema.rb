4bb65bdf33791fb688d74e93b0e44ec9eb3dea30
Lynden Lim <lyndenlim15@gmail.com>
2022-03-04 15:10:23 -0500

    Create database

---
+++ trail_blazers-server/db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_03_04_193227) do
+
+  create_table "hikers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "location"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "rating"
+    t.string "comment"
+    t.integer "hiker_id"
+    t.integer "trail_id"
+    t.date "created_date"
+    t.date "updated_date"
+  end
+
+  create_table "trails", force: :cascade do |t|
+    t.string "park_name"
+    t.string "trail_name"
+    t.string "location"
+    t.string "difficulty"
+    t.integer "duration"
+    t.string "attraction"
+  end
+
+end
