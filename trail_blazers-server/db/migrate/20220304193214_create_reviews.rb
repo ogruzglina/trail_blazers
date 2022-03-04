f598a60b2ae6fc63d40ffea1110110952cdd8c76
Lynden Lim <lyndenlim15@gmail.com>
2022-03-04 15:08:22 -0500

    Create tables and clear seed file

--- trail_blazers-server/db/migrate/20220304193214_create_reviews.rb
+++ trail_blazers-server/db/migrate/20220304193214_create_reviews.rb
@@ -1,4 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :rating
+      t.string :comment
+      t.integer :hiker_id
+      t.integer :trail_id
+      t.date :created_date
+      t.date :updated_date
+    end
   end
 end
--- trail_blazers-server/db/migrate/20220304193219_create_hikers.rb
+++ trail_blazers-server/db/migrate/20220304193219_create_hikers.rb
@@ -1,4 +1,9 @@
 class CreateHikers < ActiveRecord::Migration[6.1]
   def change
+    create_table :hikers do |t|
+      t.string :name
+      t.integer :age
+      t.string :location
+    end
   end
 end
