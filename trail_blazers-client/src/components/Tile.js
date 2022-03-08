16faa65db5f07b20aec87e765d1b1d158b389246
Lynden Lim <lyndenlim15@gmail.com>
2022-03-07 23:36:14 -0500

    Fixed link to be only on tiles

--- trail_blazers-client/src/components/Tile.js
+++ trail_blazers-client/src/components/Tile.js
@@ -6,12 +6,12 @@ import Card from "react-bootstrap/Card"
 
 function Tile({ props }) {
     return (
-        <Link to="/review">
-            <div className="tile-container">
-                <Row xs={1} md={4} className="g-4">
-                    {Array.from({ length: 20 }).map((_, idx) => (
-                        <Col>
-                            <Card>
+        <div className="tile-container">
+            <Row xs={1} md={4} className="g-4">
+                {Array.from({ length: 20 }).map((_, idx) => (
+                    <Col>
+                        <Card>
+                            <Link to="/review">
                                 <Card.Img variant="top" src="https://cdn.securem2.com//commonimages/pages/2021/8/mountainrangefall.jpg" />
                                 <Card.Body>
                                     <Card.Title>Card title</Card.Title>
@@ -19,12 +19,12 @@ function Tile({ props }) {
                                         Card body
                                     </Card.Text>
                                 </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </div>
-        </Link>
+                            </Link>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </div>
     )
 }
 
