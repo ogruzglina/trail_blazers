adcd274ac4eba08ef2264cda0d4f6866c9f9b349
Lynden Lim <lyndenlim15@gmail.com>
2022-03-09 11:44:55 -0500

    All hiker route

--- trail_blazers-server/app/controllers/application_controller.rb
+++ trail_blazers-server/app/controllers/application_controller.rb
@@ -9,22 +9,32 @@ class ApplicationController < Sinatra::Base
       count_reviews = trail.trail_count_reviews
       avg_rating = trail.trail_avg_rating
       count_stars = trail.trail_count_stars
-      trails_count_and_rating << {trail_id: trail.id, count: count_reviews, avg_review: avg_rating, count_stars: count_stars}
+      trails_count_and_rating << { trail_id: trail.id, count: count_reviews, avg_review: avg_rating, count_stars: count_stars }
     end
 
-    result = {trails: trails, rating: trails_count_and_rating}
+    result = { trails: trails, rating: trails_count_and_rating }
     result.to_json
   end
 
   get "/hikers/:trail_id" do
     trail_id = params[:trail_id]
     reviews = Trail.trail_reviews(trail_id)
-  
-    hikers_id = reviews.map {|review| review.hiker_id}.uniq
-    hikers = hikers_id.map {|hiker_id| Hiker.find(hiker_id)}
-  
-    trail_hikers = {trail_id: trail_id, hikers: hikers}
+
+    hikers_id = reviews.map { |review| review.hiker_id }.uniq
+    hikers = hikers_id.map { |hiker_id| Hiker.find(hiker_id) }
+
+    trail_hikers = { trail_id: trail_id, hikers: hikers }
     trail_hikers.to_json
+  end
+
+  get "/hikers" do
+    hikers = Hiker.all
+    hikers.to_json
+  end
+
+  post "/hikers" do
+    hiker = Hiker.create(name: params[:name], picture: params[:picture])
+    hiker.to_json
   end
 
   get "/reviews/:trail_id/?:sort?" do # ?:sort? -it's an optional parametr
@@ -34,9 +44,9 @@ class ApplicationController < Sinatra::Base
 
     t_reviews = Trail.trail_reviews(trail_id)
 
-    if sort == nil 
+    if sort == nil
       t_reviews.to_json
-    else 
+    else
       sorted_reviews = case sort
         when "newest"
           t_reviews.order(created_at: :desc)
@@ -46,12 +56,12 @@ class ApplicationController < Sinatra::Base
           t_reviews.order(rating: :desc)
         when "lowest"
           t_reviews.order(rating: :asc)
-      end
+        end
       sorted_reviews.to_json
     end
   end
 
-  post '/reviews' do
+  post "/reviews/:id" do
     review = Review.create(
       comment: params[:comment],
       rating: params[:rating],
@@ -60,6 +70,4 @@ class ApplicationController < Sinatra::Base
     )
     review.to_json
   end
-    
-  
 end
