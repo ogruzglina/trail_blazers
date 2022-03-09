a724c3d9b4a3e81a5da8c51539e2833d11115fa5
Lynden Lim <lyndenlim15@gmail.com>
2022-03-09 15:22:42 -0500

    Add logo and styling

--- trail_blazers-client/src/components/SavedTrails.js
+++ trail_blazers-client/src/components/SavedTrails.js
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom"
 import Navbar from "react-bootstrap/Navbar"
 import Container from "react-bootstrap/Container"
+import logo from "./logo.png"
 
 function SavedTrails() {
     return (
         <Navbar>
             <Container>
                 <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-                    Home
+                    <b>Home</b>
                 </Link>
-                <b style={{ fontSize: "24px" }}>Trail Blazers</b>
+                <span style={{paddingLeft: "48px"}}><img style={{ height: "100px" }} src={logo} alt="logo" /></span>
                 <span style={{ color: "white" }}>placeholder</span>
             </Container>
         </Navbar>
